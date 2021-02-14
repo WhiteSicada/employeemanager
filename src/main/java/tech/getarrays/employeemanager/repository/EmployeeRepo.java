68eafe93ca486187e95b71d9512c49a308598207
WhiteSicada <dino_villa@hotmail.fr>
2021-02-14 03:41:06 +0100

    Add Service + exception + update Repo

--- src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepo.java
+++ src/main/java/tech/getarrays/employeemanager/repository/EmployeeRepo.java
@@ -3,6 +3,11 @@ package tech.getarrays.employeemanager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.getarrays.employeemanager.model.Employee;
 
+import java.util.Optional;
+
 // the repo take 2 params the model and the the type of the primary key
 public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
